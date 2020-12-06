90e661812109d15160ac75e8c0f711375e949637
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-06 21:38:44 +0530

    [Arti Dandge] Add : Ability to analyse and respond Happy and Sad Mood

---
+++ MoodAnalyserProject/MoodAnalyser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MoodAnalyserProject
+{
+    public class MoodAnalyser
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser Project !");
+        }
+        public string analyseMood(string mood)
+        {
+            if (mood.Equals("Happy"))
+            {
+                return mood + " mood";
+            }
+            else 
+            {
+                return mood + " mood";
+            }
+        }
+    }
+}
