4ba1ccbbe64f14093e17c59ebfcfd56aa2d61539
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-08 18:31:52 +0530

    [Arti Dandge] Add : Custom Exception for empty and null message

--- MoodAnalyserProject/MoodAnalyser.cs
+++ MoodAnalyserProject/MoodAnalyser.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace MoodAnalyserProject
 {
     public class MoodAnalyser
@@ -20,6 +22,10 @@ namespace MoodAnalyserProject
         {
             try
             {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.EMPTY_MESSAGE, "Mood should not be empty");
+                }
                 if (this.message.Contains("Sad"))
                 {
                     return "SAD";
@@ -37,9 +43,9 @@ namespace MoodAnalyserProject
                     return "Happy";
                 }
             }
-            catch
+            catch(NullReferenceException)
             {
-                return "Happy";
+                throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.NULL_MESSAGE, "Message should not be null");
             }
         }
                    
